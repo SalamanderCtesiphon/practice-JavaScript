b5a76dd10fb1ae3771e8bf8a8f8af19f4ba171eb
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-14 11:43:11 -0600

    worked through the assignment

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Hey I’m red!';
+paragraph.style.color = 'red';
+
+container.appendChild(paragraph);
+
+const header3 = document.createElement('h3');
+header3.textContent = 'I’m a blue h3!';
+header3.style.color = 'blue';
+
+container.appendChild(header3);
+
+const div = document.createElement('div');
+div.style.border = '1px solid black';
+div.style.backgroundColor = 'pink';
+
+const header1 = document.createElement('h1');
+header1.textContent = 'I’m in a div';
+div.appendChild(header1);
+
+const paragraph2 = document.createElement('p');
+paragraph2.textContent = 'ME TOO!';
+div.appendChild(paragraph2);
+
+container.appendChild(div);
+
